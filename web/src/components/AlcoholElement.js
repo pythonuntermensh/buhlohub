55fbd6b39ea391e849de6dc1b918a1950c9002c6
pythonuntermensh <pythonuntermensh@gmail.com>
2022-08-24 23:53:59 +0400

    fixed temp solutions: styles naming

--- web/src/components/AlcoholElement.js
+++ web/src/components/AlcoholElement.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './styles/AlcoholElement.css';
+import './styles/CatalogElement.css';
 
 const AlcoholElement = ({ drink }) => {
     return (
-        <li className="alcohol-element-wrapper">
+        <li className="catalog-element-wrapper">
             <Link to={"/product/"+drink.id}>
-                <img height="150" width="150" src={drink.img} alt="img" className="alcohol-element-img"/>
-                <div className="alcohol-element-info">
-                    <div className="alcohol-element-name">{drink.name}</div>
-                    <div className="alcohol-element-proof">{"proof: " + drink.proof + "°"}</div>
-                    <div className="alcohol-element-cost">{"cost: " + drink.averageCost + "$"}</div>
+                <img height="150" width="150" src={drink.img} alt="img" className="catalog-element-img"/>
+                <div className="catalog-element-info">
+                    <div className="catalog-element-name">{drink.name}</div>
+                    <div className="catalog-element-proof">{"proof: " + drink.proof + "°"}</div>
+                    <div className="catalog-element-cost">{"cost: " + drink.averageCost + "$"}</div>
                 </div>
             </Link>
         </li>
--- web/src/components/ReviewElement.js
+++ web/src/components/ReviewElement.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './styles/AlcoholElement.css';
+import './styles/CatalogElement.css';
 
 const ReviewElement = ({ review }) => {
     return (
-        <li className="alcohol-element-wrapper">
+        <li className="catalog-element-wrapper">
             <Link to={"/review/"+review.id}>
-                <img height="150" width="150" src={review.drink.img} alt="img" className="alcohol-element-img"/>
-                <div className="alcohol-element-info">
-                    <div className="alcohol-element-name">{review.title}</div>
-                    <div className="alcohol-element-proof">by {review.author.username}</div>
+                <img height="150" width="150" src={review.drink.img} alt="img" className="catalog-element-img"/>
+                <div className="catalog-element-info">
+                    <div className="catalog-element-name">{review.title}</div>
+                    <div className="catalog-element-proof">by {review.author.username}</div>
                 </div>
             </Link>
         </li>
